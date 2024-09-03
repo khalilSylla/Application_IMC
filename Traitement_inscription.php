2d42f8f5f7319dec16c576fd6342aea71209fc9a
khalil <is6010625@gmail.com>
2024-09-03 15:34:11 +0000

    ajout du pattern singleton,du page de traitement du formulaire d'inscription et de la page de profil

---
+++ Traitement_inscription.php
@@ -0,0 +1,38 @@
+<?php
+require_once("db.php");
+if (isset($_POST["envoyer"])) {
+    $nom = $_POST["Nom"];
+    $prenom = $_POST["Prenom"];
+    $email = $_POST["Email"];
+    $mdp=$_POST["Mot_de_passe"] ;
+    $mdph = hash('sha256',$mdp);
+    $genre= $_POST["Genre"];
+
+    // Vérifier si l'email existe déjà
+    $checkUser = $connectionbd->prepare("SELECT * FROM utilisateur WHERE EMAIL = :email");
+    $checkUser->bindParam(':email', $email);
+    $checkUser->execute();
+
+
+    if ($checkUser->rowCount() > 0) {
+        echo "Cet utilisateur existe déjà fjkldfjkldfjklj";
+    } else {
+    $sql = ("INSERT INTO `utilisateur`(`NOM`, `PRENOM`,`EMAIL`, `MOTS_DE_PASSE`,`ID_GENRE`)
+               VALUES (:nom, :prenom, :email, :mdph , :genre)");
+    $stml = $connectionbd->prepare($sql);
+    
+    $stml->bindParam(':nom', $nom);
+    $stml->bindParam(':prenom', $prenom);
+    $stml->bindParam(':email', $email);
+    $stml->bindParam(':mdph', $mdph);
+    $stml->bindParam(':genre', $genre);
+
+    if ($stml->execute()) {
+        echo"Inscription réussie. Redirection vers la page de connexion dans 10 secondes...";
+        header("Location:connexion.html?inscription=ok");
+    } else {
+        echo "L'inscription a échoué.";
+    }
+}
+}
+?>
---
+++ db1.php
@@ -0,0 +1,14 @@
+<?php
+$serveurname = "localhost" ;
+$username = "root" ;
+$password = "" ;
+$dbname = "imc" ;
+//Etablir la connection a la base de donnes
+try {
+   $connectionbd = new PDO("mysql:host=$serveurname;dbname=$dbname", $username, $password);
+   $connectionbd->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION); 
+   //echo "Connection etablit";
+} catch (PDOException $e) {
+    echo "la connection a echoue:". $e->getMessage();
+}
+?>
